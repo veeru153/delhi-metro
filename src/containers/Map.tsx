05ef17be15e4321bc5398acabf91ccadeedadedf
veeru153 <32760644+veeru153@users.noreply.github.com>
2024-07-14 00:37:13 +0530

    Converted Map SVG to React Component

--- src/containers/Map.tsx
+++ src/containers/Map.tsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 // @ts-ignore
-import MapSvg from "../assets/map.svg?react";
+import MapSvgImg from "../assets/map.svg?react";
+import MapSvg from "../components/MapSvg";
+import { useState } from "react";
 
 
 export default function Map() {
     const navigate = useNavigate();
+    const [x, setX] = useState(-17.254000854492176);
+    const [y, setY] = useState(143.71231079101562);
+    const [scale, setScale] = useState(0.3);
+
     return <>
         <div className="flex flex-col h-screen">
             <div className="flex flex-row w-full bg-white">
@@ -19,8 +25,8 @@ export default function Map() {
                     <p className="text-xl">Map</p>
                 </div>
             </div>
-            <div className="flex flex-1 overflow-hidden">
-                <MapSvg />
+            <div className="flex flex-1">
+                <MapSvg x={x} y={y} scale={scale} />
             </div>
         </div>
     </>
