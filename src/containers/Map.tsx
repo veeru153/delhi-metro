b691b5fe2b9ae68bf07053b45a66136de5485dde
veeru153 <32760644+veeru153@users.noreply.github.com>
2024-07-14 15:54:06 +0530

    Migrated to viewbox controls instead of transforms

--- src/containers/Map.tsx
+++ src/containers/Map.tsx
@@ -3,7 +3,7 @@ import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutl
 // @ts-ignore
 import MapSvgImg from "../assets/map.svg?react";
 import MapSvg from "../components/MapSvg";
-import { useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useRef } from "react";
 
 const ORIGINAL_X = -366.2540008544922;
 const ORIGINAL_Y = -11.287689208984375;
@@ -12,9 +12,6 @@ const ORIGINAL_SCALE = 0.7
 export default function Map() {
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
-    const [x, setX] = useState(ORIGINAL_X);
-    const [y, setY] = useState(ORIGINAL_Y);
-    const [scale, setScale] = useState(ORIGINAL_SCALE);
 
     const routePath = searchParams.get("route");
     const routePathCodes: string[] = [];
@@ -37,6 +34,9 @@ export default function Map() {
 
     let isPointerDown = false;
     const pointerOrigin = { x: 0, y: 0 };
+    const viewBox = { x: 7.944935326437348, y: 84.7464617386605, w: 491, h: 758 };
+    const newViewBox = { x: 0, y: 0, w: 0, h: 0 };
+    let ratio = 1;
 
     function getPointFromEvent(event: MouseEvent | TouchEvent) {
         const point = { x: 0, y: 0 };
@@ -62,27 +62,39 @@ export default function Map() {
     function onPointerMove(event: MouseEvent | TouchEvent) {
         if (!isPointerDown) return;
         event.preventDefault();
+
         const pointerPosition = getPointFromEvent(event);
         const xDiff = pointerPosition.x - pointerOrigin.x;
         const yDiff = pointerPosition.y - pointerOrigin.y;
 
-        // TODO: Update min max based on zoom levels
-        setX(Math.max(-2700, Math.min(x + xDiff, 221)));
-        setY(y + yDiff);
-        // setY(Math.max(-2458, Math.min(y + yDiff, -1696)));
+        newViewBox.x = Math.min(648, Math.max(viewBox.x - (xDiff * ratio), -420));
+        newViewBox.y = Math.min(800, Math.max(viewBox.y - (yDiff * ratio), -36));
+
+        const viewBoxStr = `${newViewBox.x} ${newViewBox.y} ${viewBox.w} ${viewBox.h}`;
+        svgRef.current?.setAttribute("viewBox", viewBoxStr);
     }
 
-    function onPointerUp(event: MouseEvent | TouchEvent) {
+    function onPointerUp() {
         isPointerDown = false;
+        viewBox.x = newViewBox.x;
+        viewBox.y = newViewBox.y;
     }
 
     function onWheelMove(event: WheelEvent) {
         const scaleDiff = event.deltaY / 1000;
-        setScale(Math.max(0.7, Math.min(scale - scaleDiff, 2.5)));
-        if (scale === ORIGINAL_SCALE) {
-            setX(ORIGINAL_X);
-            setY(ORIGINAL_Y);
+
+        viewBox.w = Math.min(491, Math.max(viewBox.w * (1 + scaleDiff), 120));
+        viewBox.h = Math.min(758, Math.max(viewBox.h * (1 + scaleDiff), 120 * (758 / 491)));
+
+        if (viewBox.w === 491 || viewBox.h === 758) {
+            viewBox.x = 0;
+            viewBox.y = 0;
         }
+
+        const viewBoxStr = `${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}`;
+        ratio = ref.current != null ? viewBox.w / ref.current.getBoundingClientRect().width : 1;
+
+        svgRef.current?.setAttribute("viewBox", viewBoxStr);
     }
 
     useLayoutEffect(() => {
@@ -100,23 +112,29 @@ export default function Map() {
     })
 
     useLayoutEffect(() => {
-        console.log("Repainting Route");
-        const lineSelector = `g.lines > line:not(${routePathCodes.map(code => "#" + code).join(",")})`;
-        const lines = svgRef.current?.querySelectorAll(lineSelector);
-        lines?.forEach(line => line.setAttribute("stroke", "#eeeeee"));
+        if (routePath != null) {
+            const lineSelector = `g.lines > line:not(${routePathCodes.map(code => "#" + code).join(",")})`;
+            const lines = svgRef.current?.querySelectorAll(lineSelector);
+            lines?.forEach(line => line.setAttribute("stroke", "#eeeeee"));
 
-        const stationsGroup = svgRef.current?.querySelector("g.stations");
-        const stationSelector = `g:not(${routeStationCodes.map(code => "#" + code).join(",")})`;
-        const stations = stationsGroup?.querySelectorAll(stationSelector);
-        stations?.forEach(station => (station.firstChild as Element).setAttribute("stroke", "#eeeeee"));
+            const stationsGroup = svgRef.current?.querySelector("g.stations");
+            const stationSelector = `g:not(${routeStationCodes.map(code => "#" + code).join(",")})`;
+            const stations = stationsGroup?.querySelectorAll(stationSelector);
+            stations?.forEach(station => (station.firstChild as Element).setAttribute("stroke", "#eeeeee"));
 
-        const transferStationsGroup = svgRef.current?.querySelector("g.transferStations");
-        const transferStations = transferStationsGroup?.querySelectorAll(stationSelector);
-        transferStations?.forEach(station => (station.firstChild as Element).setAttribute("stroke", "#eeeeee"));
+            const transferStationsGroup = svgRef.current?.querySelector("g.transferStations");
+            const transferStations = transferStationsGroup?.querySelectorAll(stationSelector);
+            transferStations?.forEach(station => (station.firstChild as Element).setAttribute("stroke", "#eeeeee"));
 
-        const interchangesGroup = svgRef.current?.querySelector("g.interchanges");
-        const interchanges = interchangesGroup?.querySelectorAll(stationSelector);
-        interchanges?.forEach(station => (station.firstChild as Element).setAttribute("stroke", "#eeeeee"));
+            const interchangesGroup = svgRef.current?.querySelector("g.interchanges");
+            const interchanges = interchangesGroup?.querySelectorAll(stationSelector);
+            interchanges?.forEach(station => (station.firstChild as Element).setAttribute("stroke", "#eeeeee"));
+        }
+
+        ratio = ref.current != null ? viewBox.w / ref.current.getBoundingClientRect().width : 1;
+        window.addEventListener('resize', function () {
+            ratio = ref.current != null ? viewBox.w / ref.current.getBoundingClientRect().width : 1;
+        });
     }, [])
 
     return <>
@@ -132,8 +150,8 @@ export default function Map() {
                     <p className="text-xl">Map</p>
                 </div>
             </div>
-            <div className="flex flex-1 justify-center items-center" style={{ maxHeight: "90vh" }} ref={ref}>
-                <MapSvg x={x} y={y} scale={scale} ref={svgRef} />
+            <div className="flex flex-1 justify-center items-center overflow-hidden" ref={ref}>
+                <MapSvg x={ORIGINAL_X} y={ORIGINAL_Y} scale={ORIGINAL_SCALE} ref={svgRef} />
             </div>
         </div>
     </>
